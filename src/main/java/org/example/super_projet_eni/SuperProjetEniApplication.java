75ea8d7fd722a01463d3df056c5f2e922b40f3b5
rbourgeon2025 <romain.bourgeon2025@campus-eni.fr>
2025-06-10 14:22:04 +0200

    Début

---
+++ src/main/java/org/example/super_projet_eni/SuperProjetEniApplication.java
@@ -0,0 +1,13 @@
+package org.example.super_projet_eni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperProjetEniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperProjetEniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/super_projet_eni/SuperProjetEniApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.super_projet_eni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperProjetEniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
