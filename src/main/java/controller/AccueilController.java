779203f18b059be1a75c6711b18406fbfd520081
rbourgeon2025 <romain.bourgeon2025@campus-eni.fr>
2025-06-11 15:35:40 +0200

    HTML 1.2

--- src/main/java/controller/AccueilController.java
+++ src/main/java/controller/AccueilController.java
@@ -8,18 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AccueilController {
 
     @GetMapping("/")
-    public String accueil(Model model) {
-        model.addAttribute("pageTitle", "marteau.png");
+    public String accueil() {
         return "index";
     }
 
-    @GetMapping("/about")
-    public String about() {
-        return "about"; // crée about.html plus tard si tu veux
-    }
-
-    @GetMapping("/contact")
-    public String contact() {
-        return "contact"; // crée contact.html plus tard
-    }
 }
